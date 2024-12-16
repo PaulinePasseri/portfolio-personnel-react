88cc3cd775d589222baaf2f9f83c0131126dbd2f
Pauline Passeri <paulinecpasseri@gmail.com>
2024-12-16 18:44:21 +0100

    Particules fond d'écran

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css'
+import ParticlesBackground from './Components/ParticlesBackground/ParticlesBackground'
+
+export default function App({ children }) {
+    return (
+        <div className='App'>
+            <ParticlesBackground id='particles'/>
+            {children}
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import App from './App'
 import Header from './Components/Header/header'
 import Footer from './Components/Footer/footer'
 import Home from './Pages/Home/home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </Router>
-  </React.StrictMode>
+    <App>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </App>
+  </React.StrictMode>,
 )
