0e71902bf03ed5ed6928b64aaefec3882eb0f483
Pauline Passeri <paulinecpasseri@gmail.com>
2024-12-20 14:54:25 +0100

    header peaufiné

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import MenuItem from './MenuItem'; 
 import './header.css';
+import Logo from '../../Assets/pauline.png'
 
 export default function Header() {
     const menuItems = [
-        { text: "Pauline Passeri", path: "/" },
+        { text: "Accueil", path: "/" },
         { text: "Projets", path: "/projets" },
-        { text: "Contact", path: "/contact" },
         { text: "Compétences", path: "/competences" },
         { text: "Formations", path: "/formations" },
-        { text: "Mon CV", path: "/cv" }
+        { text: "Contact", path: "/contact" }
     ];
 
     return (
         <nav>
+           <img src={Logo} alt='logo portfolio' className='logo'/> 
             <ul>
                 {menuItems.map((item, index) => (
                     <li key={index}>
@@ -24,6 +25,7 @@ export default function Header() {
                     </li>
                 ))}
             </ul>
+           <button className='btn'>MON CV</button> 
         </nav>
     );
 }
