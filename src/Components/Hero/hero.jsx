e43575382520b27953375cac0708f9693d6b2e1c
Pauline Passeri <paulinecpasseri@gmail.com>
2024-12-22 16:16:20 +0100

    Ajout lune animée dans le hero

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -3,7 +3,11 @@ import './hero.css'
 export default function Hero() {
     return(
         <section className='hero-container'>
-            <h1>Bonjour,<br/>Je suis Pauline Passeri.</h1>
+            <div className='hero-container__txt'>
+                <h1>Bonjour,<br/>Je suis Pauline Passeri.</h1>
+                <p>Web developer</p>
+            </div>
+            <div className='animated-moon'></div>
         </section>
         
     )
