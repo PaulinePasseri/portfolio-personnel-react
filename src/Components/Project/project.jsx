9bfbff8a0c594027026831dda293256cc9193055
Pauline Passeri <paulinecpasseri@gmail.com>
2025-01-05 20:50:04 +0100

    base du composant projet

--- src/Components/Project/project.jsx
+++ src/Components/Project/project.jsx
@@ -1,9 +1,20 @@
 import './project.css';
 
-export default function Project({ image }) {
+export default function Project({ image, title, description, competences }) {
     return (
-        <button className="project-button">
-            <img src={image} alt="projet" />
+        <button className='project-button'>
+            <h2>{title}</h2>
+            <div className='project-img'>
+                <img src={image} alt="projet" />
+            </div>
+            <div className='trapezoid-1'></div>
+            <div className='trapezoid-2'></div>
+            <div className='project-txt'>
+                <h3>[ DESCRIPTION ] </h3>
+                <p>{description}</p>
+                <h3>[ COMPETENCES ]</h3>
+                <p>/ {competences}</p>
+            </div>
         </button>
     );
 }
