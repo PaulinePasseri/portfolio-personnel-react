a6323cd0e36dc4613761e689886b9541174af552
Pauline Passeri <paulinecpasseri@gmail.com>
2025-01-10 01:00:53 +0100

    ajout d'une bio

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -17,6 +17,11 @@ export default function Home() {
             <div className={`home-txt ${isVisible ? 'animate' : ''}`}>
                 <h1>Bonjour<br />Je suis Pauline Passeri</h1>
                 <strong className="text-highlight">Développeuse frontend</strong>
+                <p className='home-bio'>
+                Après un diplôme d'ingénieur, j'ai fait le choix de me réorienter vers le développement web, un domaine qui me passionne profondément. 
+                <br />
+                Mon objectif est de créer des applications intuitives et innovantes qui répondent aux besoins des utilisateurs tout en restant à la pointe des technologies.
+                </p>
             </div>
             <AnimatedPlanet image={Moon} animate={isVisible}/> 
         </div>
