85ec9288dc64dba8c9374f3f98ada8af8ac510dd
Pauline Passeri <paulinecpasseri@gmail.com>
2025-02-02 23:42:46 +0100

    amélioration UX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import ParticlesBackground from './Components/ParticlesBackground/ParticlesBackground';
-import CustomCursor from './Components/CustomCursor/CustomCursor'; 
 
 export default function App({ children }) {
     return (
         <div className='App'>
             <ParticlesBackground id='particles'/>
             {children} 
-            <CustomCursor /> 
         </div>
     );
 }
